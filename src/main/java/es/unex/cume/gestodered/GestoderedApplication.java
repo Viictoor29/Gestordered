fcf24bca78738e795d6e820d072c07c4bf524c9d
victoor29 <vicpincor29@gmail.com>
2026-03-03 23:47:55 +0100

    Starting proyect TFG_Informatica

---
+++ src/main/java/es/unex/cume/gestodered/GestoderedApplication.java
@@ -0,0 +1,13 @@
+package es.unex.cume.gestodered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoderedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoderedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/unex/cume/gestodered/GestoderedApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.cume.gestodered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoderedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
