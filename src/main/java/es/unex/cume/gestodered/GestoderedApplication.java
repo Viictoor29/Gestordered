c8e49560c9a7248a48470f5abbcbe58b1a1f71dd
victoor29 <vicpincor29@gmail.com>
2026-05-23 03:03:44 +0200

    refactor: Remove password visibility toggle and related styles from login form

--- src/main/java/es/unex/cume/gestodered/GestoderedApplication.java
+++ src/main/java/es/unex/cume/gestodered/GestoderedApplication.java
@@ -1,14 +1,7 @@
 package es.unex.cume.gestodered;
 
-import es.unex.cume.gestodered.data.model.User;
-import es.unex.cume.gestodered.data.repository.UserRepository;
-import org.bson.Document;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class GestoderedApplication {
